ce264e7fa49541b06af0d145bd5b50430f32f31b
JorimJoram <wkdgyfla97@gmail.com>
2025-09-08 00:47:10 +0900

    fix: layout overflow

--- lib/features/authentication/signin/presentation/signin_screen.dart
+++ lib/features/authentication/signin/presentation/signin_screen.dart
@@ -22,22 +22,30 @@ class _SigninScreenState extends State<SigninScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(child: const Center()),
-              Expanded(
-                child: SigninForm(
-                  usenameController: _usernameController,
-                  passwordController: _passwordController,
-                  onPressedAuth: _handleAuth,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SigninForm(
+                        usenameController: _usernameController,
+                        passwordController: _passwordController,
+                        onPressedAuth: _handleAuth,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Expanded(child: const Center()),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
